f82b0d5a3d1054ab3f304eb3030b1d93e84a837d
Enkata DevOps <gakrchintugmailcom@example.com>
2025-07-10 19:57:48 +0000

    Fix: use Docker from shim instead of hard path

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "Running Docker Build and Container..."
 
-# Use full path to Docker binary
-/usr/bin/docker build -t signup-app .
+docker build -t signup-app .
 
-/usr/bin/docker rm -f signup || true
+docker rm -f signup || true
 
-/usr/bin/docker run -d -p 5000:5000 --name signup signup-app
+docker run -d -p 5000:5000 --name signup signup-app
